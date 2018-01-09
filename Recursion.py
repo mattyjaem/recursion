f138b12fb45e5ae29440d5334fb13770d3260c6c
jacko8638 <jdowning@kingsworcester.org.uk>
2018-01-09 09:39:07 +0000

    Initial commit

---
+++ Recursion.py
@@ -0,0 +1,20 @@
+import math
+def sumEven(n):
+    if int(n) > 0:
+        if int(n) % 2 == 0:
+            n = int(n) + sumEven(int(n) - 2)
+            return(n)
+        else:
+            print(n)
+            n = int(n) - 1
+            n = int(n) + sumEven(int(n) - 2)
+            return(n)
+    else:
+        return(n)
+
+#Main program
+while 1 == 1:
+    number = input("Enter Number:")
+    sum = sumEven(number)
+    print ("The Sum of the evens up to that number is: %s" %sum)
+
---
+++ recursive routine.py
@@ -0,0 +1,5 @@
+def calc (n):
+    if n > 0:
+        n = n + calc (n - 1)
+    elif:
+        return n
