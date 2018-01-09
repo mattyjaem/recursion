64c0fb971e071ce535f1dc47b782ba62d95a389e
jacko8638 <jdowning@kingsworcester.org.uk>
2018-01-09 14:33:33 +0000

    Initial commit

---
+++ Numofbarcodes.py
@@ -0,0 +1,11 @@
+Barcodes = []
+NumOfBarcodes = 0
+while NumOfBarcodes <= 100:
+	Barcode = input(“Enter barcode: “)
+	Barcodes.append(barcode)
+for i  in barcodes:
+    Barcode = barcodes[i]
+	Weights = 13131
+	Multweights = “”
+for x in barcode:
+		multWeights = “multweights” + barcode[x] * weights[x]
